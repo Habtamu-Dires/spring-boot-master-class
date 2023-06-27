2067b4f7db39f9c90c7f76b4d1056b5669fbb195
Habtamu <habtamuadugna.d@gmail.com>
2023-06-27 17:19:47 +0300

    accessing application.properties programmatically

--- src/main/java/com/example/demo/customer/CustomerConfiguration.java
+++ src/main/java/com/example/demo/customer/CustomerConfiguration.java
@@ -1,19 +1,34 @@
 package com.example.demo.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import com.example.demo.infoapp.infoApp;
 
 @Configuration
 public class CustomerConfiguration {
     @Value("${app.useFakeCustomerRepo:false}")
     private Boolean useFakeCustomerRepo;
 
+    @Value("${info.company.name}")
+    private String companyName;
+
+    @Autowired
+    private Environment environment;
+
     @Bean  
-    CommandLineRunner commandLineRunner(){
+    CommandLineRunner commandLineRunner(infoApp infoApp){
         return args ->{
             System.out.println("Command Liner runner hooray");
+            System.out.println(companyName);
+            System.out.println(
+                environment.getProperty("info.app.version")
+            );
+            System.out.println(infoApp);
         };
     }
 
